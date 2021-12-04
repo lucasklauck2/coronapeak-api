0fe79434358d4b65729f3aef13b324df4436b9ce
Lucas Klauck <lucasklauck2@gmail.com>
2021-12-04 16:07:15 -0300

    Avanços projeto corona-peak

--- src/main/java/br/com/coronapeak/repository/PacienteRepository.java
+++ src/main/java/br/com/coronapeak/repository/PacienteRepository.java
@@ -21,12 +21,12 @@ public interface PacienteRepository extends JpaRepository<Paciente, Long>{
 	@Query("select new br.com.coronapeak.dto.DadosRelatorioUmDTO(P.id, P.nome) from Paciente as P where cast(extract(year from age(CURRENT_DATE, P.dataNascimento)) as integer) % 2 = 1 and exists (select PS from PacienteSintoma as PS join Sintoma as S on S.id = PS.codigoSintoma where PS.codigoPaciente = P.id and S.nome = 'Febre')")
 	public abstract List<DadosRelatorioUmDTO> adquirirRelatorioUm();
 	
-	@Query("select new br.com.coronapeak.dto.DadosRelatorioDoisDTO(P.nome , C.nome) from Paciente as P join Cidade C on C.id = P.cidade join Questionario Q on Q.id = P.questionario where Q.flagPositivado = true and (select count(*) from PacienteSintoma PS where PS.codigoPaciente = P.id) > 0 and C.nome in ('Maravilha', 'Descanso', 'Pinhalzinho', 'Chapecó', 'Itapiranga') order by C.nome asc, P.nome desc")
+	@Query("select new br.com.coronapeak.dto.DadosRelatorioDoisDTO(P.nome , C.nome) from Paciente as P join Cidade C on C.id = P.cidade join Questionario Q on Q.id = P.questionario where Q.flagPositivado = true and (select count(*) from PacienteSintoma PS where PS.codigoPaciente = P.id) > 0 and C.nome in ('Maravilha', 'Descanso', 'Pinhalzinho', 'Chapecó', 'Itapiranga') and P.peso > 60  order by C.nome asc, P.nome desc")
 	public abstract List<DadosRelatorioDoisDTO> adquirirRelatorioDois();
 	
-	@Query("select new br.com.coronapeak.dto.DadosRelatorioTresDTO(C.id, C.nome, count(*) as qt_susp) from Paciente P join Questionario Q on Q.id = P.questionario join Cidade C on C.id = P.cidade where Q.flagSuspeito = true and C.id = P.cidade group by C.id having count(*) > 0 order by qt_susp desc")
+	@Query("select new br.com.coronapeak.dto.DadosRelatorioTresDTO(C.id, C.nome, count(*) as qt_susp) from Paciente P join Questionario Q on Q.id = P.questionario join Cidade C on C.id = P.cidade where Q.flagSuspeito = true and C.id = P.cidade group by C.id having count(*) < 10 order by qt_susp desc")
 	public abstract List<DadosRelatorioTresDTO> adquirirRelatorioTres();
 	
-	@Query("select new br.com.coronapeak.dto.DadosRelatorioQuatroDTO(extract(year from age(CURRENT_DATE, P.dataNascimento)), count(*) as qt_casos) from Paciente P inner join Questionario Q on Q.id = P.questionario  where Q.flagPositivado = true group by extract(year from age(CURRENT_DATE, P.dataNascimento)) having count(*) > 0 order by qt_casos desc")
+	@Query("select new br.com.coronapeak.dto.DadosRelatorioQuatroDTO(extract(year from age(CURRENT_DATE, P.dataNascimento)), count(*) as qt_casos) from Paciente P inner join Questionario Q on Q.id = P.questionario  where Q.flagPositivado = true group by extract(year from age(CURRENT_DATE, P.dataNascimento)) having count(*) > 20 order by qt_casos desc")
 	public abstract List<DadosRelatorioQuatroDTO> adquirirRelatorioQuatro();
 }
